ad178da4385199d0197bb041e9cf4af5c6e8a195
Sri Pardhu Shanmuk <pardhupiano@gmail.com>
2026-03-18 02:37:44 +0530

    Update settings.py

--- bloodline_bd/settings.py
+++ bloodline_bd/settings.py
@@ -150,3 +150,5 @@ STATIC_URL = 'static/'
 CORS_ALLOW_CREDENTIALS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
